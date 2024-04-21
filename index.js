1233177367b9cf0692f6f9ee72707a37f4950f4d
Omkara07 <singh.omkara07@gmail.com>
2024-04-21 14:19:41 +0530

    update 4

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 const connectTOMongo = require('./db')
 const express = require('express')
 var cors = require('cors')
-const axios  = require('axios')
+// const axios  = require('axios')
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: "https://notebook-frontend-five.vercel.app"
+}))
 connectTOMongo();
 
 const port = 5000
 
 app.use(express.json())
-app.use(cors())
 
-app.get('/api/:endpoint*', async (req, res) => {
-  try {
-    const endpoint = req.params.endpoint + (req.params[0] || '');
-    const response = await axios.get(`https://api.rawg.io/api/${endpoint}`, {
-      params: { ...req.query, key: process.env.VITE_GAMES_API_KEY },
-    });
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ error: `${error.message}` });
-  }
-});
+// app.get('/api/:endpoint*', async (req, res) => {
+//   try {
+//     const endpoint = req.params.endpoint + (req.params[0] || '');
+//     const response = await axios.get(`https://api.rawg.io/api/${endpoint}`, {
+//       params: { ...req.query, key: process.env.VITE_GAMES_API_KEY },
+//     });
+//     res.json(response.data);
+//   } catch (error) {
+//     res.status(500).json({ error: `${error.message}` });
+//   }
+// });
 
 // available routes
 app.use('/api/auth', require('./routes/auth'));
